4dbe1fe349cb243cddcfefa35f2c000619a9d000
nick <59822256+Archasion@users.noreply.github.com>
2023-11-28 16:07:23 +0000

    refactor(exports-test): Rename param

--- tests/exports.test.ts
+++ tests/exports.test.ts
@@ -1,3 +1,5 @@
+import { AbstractInstanceType } from "../src/utils/types.ts";
+import { Snowflake } from "discord-api-types/v10";
 import { expect, test, describe } from "bun:test";
 
 import fs from "fs";
@@ -6,20 +8,27 @@ import Command from "../src/handlers/commands/Command";
 import Component from "../src/handlers/components/Component";
 import EventListener from "../src/handlers/events/EventListener";
 
-describe("exports", async (): Promise<void> => {
+const customIDs: Snowflake[] = [];
+
+describe("exports", () => {
     verifyModule("components", Component);
     verifyModule("commands", Command);
     verifyModule("events", EventListener);
 });
 
-function verifyModule(dirName: string, expectedClass: Function): void {
-    const modulesDirectoryPath = path.resolve(__dirname, "../src", dirName);
+function verifyModule(dirname: string, expectedClass: Function): void {
+    const modulesDirectoryPath = path.resolve(__dirname, "../src", dirname);
     const moduleFiles = fs.readdirSync(modulesDirectoryPath);
 
-    test.each(moduleFiles)(`${dirName}: %s`, moduleFile => {
+    test.each(moduleFiles)(`${dirname}: %s`, moduleFile => {
         const moduleFilePath = path.resolve(modulesDirectoryPath, moduleFile);
         const module = require(moduleFilePath).default;
 
         expect(Object.getPrototypeOf(module)).toStrictEqual(expectedClass);
+
+        const instance: AbstractInstanceType<typeof Command | typeof Component> = new module();
+
+        if (instance instanceof Component) customIDs.push(instance.customId);
+        if (instance instanceof Command) customIDs.push(instance.data.name);
     });
 }
